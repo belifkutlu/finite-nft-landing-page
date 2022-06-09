3957d6a0e349e9c9e6e2821dbd140a34c9ccf905
belifkutlu <elifkutlu41@gmail.com>
2022-06-09 14:46:06 +0300

    add header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import "./App.css";
+import image from "./assets/images/finite-logo.svg";
+import heroImage from "./assets/images/hero-image.png";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Elif</h1>
+    <div className="container">
+      <header className="header">
+        <div className="header-left">
+          <img src={image} alt="IMG" />
+          <input
+            placeholder="Search for “crypto punks”"
+            name="search"
+            type="search"
+          />
+        </div>
+        <navbar className="header-right">
+          <a href="/">Explore</a>
+          <a href="/">Create</a>
+          <a href="/">Blog</a>
+          <button>Connect Wallet</button>
+        </navbar>
+      </header>
+      <div className="section-1">
+        <div className="content-texts-wrapper">
+          <h1>Explore, Collect Sell Incredible NFTs</h1>
+          <p>
+            The largest NFT marketplace. Authentic and incredible unique digital
+            creations.
+          </p>
+          <div className="buttons">
+            <button>Get Started</button>
+            <button>Play Video</button>
+          </div>
+        </div>
+        <div className="hero-image">
+          <img src={heroImage} alt="hero image" />
+        </div>
+      </div>
     </div>
   );
 }
