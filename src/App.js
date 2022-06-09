f41822ef7df25bfdbc6be0ad620c7e5d740e28fb
belifkutlu <elifkutlu41@gmail.com>
2022-06-09 23:32:13 +0300

    add search input icon

--- src/App.js
+++ src/App.js
@@ -8,11 +8,36 @@ function App() {
       <header className="header">
         <div className="header-left">
           <img src={image} alt="IMG" />
-          <input
-            placeholder="Search for “crypto punks”"
-            name="search"
-            type="search"
-          />
+          <div className="input-wrapper">
+            <input
+              placeholder="Search for “crypto punks”"
+              name="search"
+              type="search"
+            />
+            <svg
+              className="search-icon"
+              width="30"
+              height="31"
+              viewBox="0 0 30 31"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M13.9081 23.7354C19.2144 23.7354 23.516 19.4337 23.516 14.1274C23.516 8.82114 19.2144 4.51953 13.9081 4.51953C8.60178 4.51953 4.30017 8.82114 4.30017 14.1274C4.30017 19.4337 8.60178 23.7354 13.9081 23.7354Z"
+                stroke="#49536E"
+                stroke-width="2.74512"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M20.7021 20.9216L26.2609 26.4805"
+                stroke="#49536E"
+                stroke-width="2.74512"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </div>
         </div>
         <navbar className="header-right">
           <a href="/">Explore</a>
@@ -60,7 +85,7 @@ function App() {
           </div>
         </div>
         <div className="hero-image">
-          <img src={heroImage} alt="hero image" />
+          <img src={heroImage} alt="hero" />
         </div>
       </div>
     </div>
